f7874c60a5e764286b552b04cf95f36de3967223
Pankajashree <pankajashree@gmail.com>
2017-06-19 14:34:13 +0530

    Week 3 video 3

--- week3/app-queryFromDOM.js
+++ week3/app-queryFromDOM.js
@@ -1,13 +1,40 @@
-var MongoClient = require('mongodb').MongoClient,
+var express = require('express'),
+    app = express(),
+    engines = require('consolidate'),
+    bodyParser = require('body-parser'),
+    MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
 
-//get query from browser form. 
-var options = constructOptions();
+var options = {};
+
+//load template
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+// Handler for internal server errors
+function errorHandler(err, req, res, next) {
+    console.error(err.message);
+    console.error(err.stack);
+    res.status(500).render('error_template', { error: err });
+}
 
 MongoClient.connect('mongodb://localhost:27017/crunchbase', function(err, db) {
 
     assert.equal(err, null);
     console.log("Successfully connected to MongoDB.");
+
+    app.get('/', function(req, res, next) {
+        res.render('query_db', {});
+    });
+
+    app.post('/query_db', function(req, res, next) {
+        //get query from browser form. 
+       options.firstYear = req.body.fy;
+        options.lastYear = req.body.ly;
+        options.employees = req.body.num;
+   
 
     var query = queryDocument(options);
     var projection = {"name" : 1, "_id" : 0,
@@ -28,7 +55,14 @@ MongoClient.connect('mongodb://localhost:27017/crunchbase', function(err, db) {
             return db.close();
         }
     );
+ });
 
+ app.use(errorHandler);
+    
+    var server = app.listen(3000, function() {
+        var port = server.address().port;
+        console.log('Express server listening on port %s.', port);
+    });
 });
 
 function queryDocument(options){
