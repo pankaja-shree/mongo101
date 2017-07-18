486eab3805ce723087fdb2839b27008314af4d24
Pankajashree <pankajashree@gmail.com>
2017-07-18 19:18:11 +0530

    add lab 5 solution

--- mongomart/cart.js
+++ mongomart/cart.js
@@ -37,20 +37,12 @@ function CartDAO(database) {
         * callback function.
         *
         */
-
-        var userCart = {
-            userId: userId,
-            items: []
-        }
-        var dummyItem = this.createDummyItem();
-        userCart.items.push(dummyItem);
-
-        // TODO-lab5 Replace all code above (in this method).
-
-        // TODO Include the following line in the appropriate
-        // place within your code to pass the userCart to the
-        // callback.
-        callback(userCart);
+        
+        this.db.collection('cart').findOne({'userId': userId},
+            (err, userCart) => {
+            assert.equal(err,null);
+            callback(userCart);
+        });
     }
 
 
